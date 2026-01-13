e1795d7194a5d2660d1f614ac83235078ec969cf
Blackbird12356 <moneyup1925@gmail.com>
2026-01-13 18:40:44 +0500

    update

--- test2/ayala/ayala/settings.py
+++ test2/ayala/ayala/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'list_of_helps.apps.ListOfHelpsConfig',
-    'bootstrap5',
+    'django_bootstrap5',
 ]
 
 
